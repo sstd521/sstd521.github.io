5b6322b6aae058a96eb8b65ed0c4227c18c923c2
sstd521 <sstd521@gmail.com>
2022-09-18 23:44:35 +0800

    1

---
+++ web-mobile/src/settings.bf4df.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/未命名.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",main:"e23f9"}};
